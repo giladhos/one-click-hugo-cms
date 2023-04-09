c5341c58037b3875df476eeca0a6dfc25d651dd3
unknown <giladhos@post.bgu.ac.il>
2023-04-09 23:19:00 +0300

    Change many things

--- cypress/e2e/basic.cy.js
+++ cypress/e2e/basic.cy.js
@@ -4,7 +4,7 @@ describe('empty spec', () => {
   })
 
   it('opens the index page', () => {
-    cy.get('h1').contains('Great coffee with a conscience')
+    cy.get('h1').contains('Try out GTP-4')
   })
 
   it('navigates to the product page', () => {
